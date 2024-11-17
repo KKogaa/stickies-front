79e3979410d2aeca8075e9da50180abf230ec2cb
KKogaa <a20160037@pucp.edu.pe>
2024-11-16 20:49:38 -0500

    added protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
 import LogIn from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import { useAuth } from "./providers/AuthProvider";
+
+const ProtectedRoute: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { isLoggedIn } = useAuth();
+
+  return isLoggedIn ? (
+    <>{children}</>
+  ) : (
+    <Navigate to="/login" replace />
+  );
+};
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} />
         <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
